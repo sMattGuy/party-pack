eebaf4c505e474ad9e21a227fe91274ae3a86c1e
MattGuy2 <matthewfrankflammia@gmail.com>
2021-12-12 13:44:42 -0500

    fixed issue with disconnect

--- partypack.js
+++ partypack.js
@@ -161,3 +161,7 @@ function resetBalance(){
 }
 // Log our bot in using the token from https://discord.com/developers/applications
 client.login(`${credentials.token}`);
+
+client.on('error', error => {
+	console.log(error);
+});
